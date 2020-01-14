74ca67c6bdf4990674b15dff975f69caeee676a0
Aysel Rzayeva <ayselrzayeva@Admins-MacBook-Pro.local>
2020-01-14 14:37:33 +0000

    looping numbers

--- __pycache__/comparisons.py
+++ __pycache__/comparisons.py
@@ -50,14 +50,29 @@
 
 
 
-current_users = ['Eric', 'Max', 'Ali']
-new_users = ['Aysel', 'Max', "Jil"]
-for new_user in new_users:
-    if new_user in current_users:
-        print(f"{new_user} have been used")
-    else:
-        print(f"{new_user} welcome")
+# current_users = ['Eric', 'Max', 'Ali']
+# new_users = ['Aysel', 'Max', "Jil"]
+# for new_user in new_users:
+#     if new_user in current_users:
+#         print(f"{new_user} have been used")
+#     else:
+#         print(f"{new_user} welcome")
 
+
+
+
+
+numbers = list(range(1, 10))
+
+for number in numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(f"{number}th")
 
 
 
