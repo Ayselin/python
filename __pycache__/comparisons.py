af289012855bba930478c74df6eb9bd572431655
Aysel Rzayeva <ayselrzayeva@Admins-MacBook-Pro.local>
2020-01-14 14:12:18 +0000

    for loop function created

--- __pycache__/comparisons.py
+++ __pycache__/comparisons.py
@@ -12,3 +12,55 @@
 #     print("Welcome")
 # else:
 #     print('bye bye')
+
+
+
+
+# city = 'Baku'
+# print("is city == 'Baku' ? Let's go")
+# print(city == 'London')
+
+# print("\nIf city == 'Baku' ? Let's explore")
+# print(city == 'Baku')
+
+
+
+
+# alien_color = 'green'
+# if alien_color == 'green':
+#     print("Player earnd 5 points")
+# elif alien_color == 'red':
+#     print("Player erned 2 points")
+# else:
+#     print("no points for you")
+
+
+
+
+# names = ['admin', 'developer', 'devop', 'engineer']
+# if 'developer' in names:
+#     print("hello")
+# if 'devop' in names:
+#     print("Hi")
+# if 'engineer' in names:
+#     print("How are you")
+# if 'admin' in names:
+#     print("Welcome admin")
+
+
+
+
+current_users = ['Eric', 'Max', 'Ali']
+new_users = ['Aysel', 'Max', "Jil"]
+for new_user in new_users:
+    if new_user in current_users:
+        print(f"{new_user} have been used")
+    else:
+        print(f"{new_user} welcome")
+
+
+
+
+
+
+
