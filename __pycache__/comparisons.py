78fb1f0f5fa6a22fe7c06e3fa91a524097b5d255
Aysel Rzayeva <ayselrzayeva@Admins-MacBook-Pro.local>
2020-01-14 11:51:16 +0000

    conditional statements

---
+++ __pycache__/comparisons.py
@@ -0,0 +1,14 @@
+# letters = ['a', 'b', 'c', 'd']
+# for letter in letters:
+#     if letter == 'b':
+#         print(letter.upper())
+#     else:
+#         print(letter)
+
+
+
+# person = 'artist'
+# if person == 'singer':
+#     print("Welcome")
+# else:
+#     print('bye bye')
